53e43e6848ea15fbbf383990f321b1db758822ef
Vladyslav Burdeniuk <vladyslavb@spotify.com>
2023-12-13 18:31:59 -0800

    Adding custom schematics

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, RecipesModule],
   controllers: [AppController],
   providers: [AppService],
 })
